3727cca27cdff3cb431466b0e52e0003d253d95d
Jessica Rozendaal <remyenjessica@gmail.com>
2022-03-18 21:30:36 +0100

    opdrachten-gemaakt

--- src/App.js
+++ src/App.js
@@ -1,50 +1,54 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from "axios";
+import Pokemon from "./components/Pokemon";
 
 function App() {
 
-   const [pokemon, setPokemon] = useState('');
+    const [pokemon, setPokemon] = useState(null);
+    const [endpoint, setEndpoint] = useState('https://pokeapi.co/api/v2/pokemon')
 
-   useEffect(() => {
-       async function fetchData() {
-           try {
-               const result = await axios.get('https://pokeapi.co/api/v2/pokemon/jigglypuff')
-               console.log(result.data);
-               setPokemon(result.data);
-           }catch (e) {
-               console.error(e);
-           }
-       }
-
-       fetchData();
-   },[]);
-
-    return (
-    <>
-        {pokemon &&
-            <>
-                <h3>{pokemon.name}</h3>
-                <img src={pokemon.sprites.front_default} alt="pokemon"/>
-                <p><strong>Moves:</strong> {pokemon.moves.length}</p>
-                <p><strong>Weight:</strong> {pokemon.weight}</p>
-                <p><strong>Abilities:</strong></p>
-                <ul>
-                    <li>
-                        {pokemon.abilities[0].ability.name}
-                    </li>
-                    <li>
-                        {pokemon.abilities[1].ability.name}
-                    </li>
-                    <li>
-                        {pokemon.abilities[2].ability.name}
-                    </li>
-                </ul>
-            </>
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const result = await axios.get(`${endpoint}`)
+                console.log(result.data);
+                setPokemon(result.data);
+            }catch (e) {
+                console.error(e);
+            }
         }
 
-    </>
-  );
+        fetchData();
+    },[endpoint]);
+
+    return (
+      <div>
+          {pokemon &&
+          <>
+              <button
+                  type="button"
+                  disabled={pokemon.previous === null}
+                  onClick={() => setEndpoint(pokemon.previous)}
+              >
+                  Previous
+              </button>
+              <button
+                  type="button"
+                  onClick={() => setEndpoint(pokemon.next)}
+              >
+                  Next
+              </button>
+              <div className="pokemon-box">
+                  {pokemon &&
+                  pokemon.results.map((pokemonCard) => {
+                      return <Pokemon key={pokemonCard.name} endpoint={pokemonCard.url}/>
+                  })}
+              </div>
+          </>
+          }
+      </div>
+);
 }
 
 export default App;
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 
-const Pokemon = () => {
+const Pokemon = ({endpoint}) => {
+    const [pokemon, setPokemon] = useState('');
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const result = await axios.get(`${endpoint}`)
+                setPokemon(result.data);
+            }catch (e) {
+                console.error(e);
+            }
+        }
+
+        fetchData();
+    },[]);
+
     return (
         <div>
-            
+            {pokemon &&
+            <>
+                <h3>{pokemon.name}</h3>
+                <img src={pokemon.sprites.front_default} alt={`The pokemon ${pokemon.name}`}/>
+                <p><strong>Moves:</strong> {pokemon.moves.length}</p>
+                <p><strong>Weight:</strong> {pokemon.weight}</p>
+                <p><strong>Abilities:</strong></p>
+                <ul>
+                    {pokemon.abilities.map((abilityNames) => {
+                        return(<li key={abilityNames.slot}>
+                                {abilityNames.ability.name}
+                            </li>
+                        )
+                    })
+                    }
+                </ul>
+            </>
+            }
+
         </div>
     );
 };
